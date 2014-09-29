bfc89e58504d248606aae1765097bf474af5371b
Maxence <maxence@spotistic.com>
2014-09-29 15:28:42 +0200

    Added: metadata.rb

---
+++ opsworks_sidekiq/metadata.rb
@@ -0,0 +1,6 @@
+name             'sidekiq'
+maintainer       'Maxence Decrosse'
+maintainer_email 'maxence@spotistic.com'
+license          'All rights reserved'
+description      'Runs sidekiq'
+version          '0.0.1'
