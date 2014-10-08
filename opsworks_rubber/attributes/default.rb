97f5f2aeb063a6685b62b6b5022875954812e1f5
Maxence <maxence@spotistic.com>
2014-10-08 18:34:39 +0200

    Fixed: start if trying to restart a non existing app

--- opsworks_rubber/attributes/default.rb
+++ opsworks_rubber/attributes/default.rb
@@ -4,7 +4,7 @@ default[:rubber] = {}
 default[:rubber][:commands] = {
   :start => 'sudo start rubber',
   :stop => 'sudo stop rubber',
-  :restart => 'sudo restart rubber'
+  :restart => 'sudo start rubber || sudo restart rubber'
 }
 default[:rubber][:config] = {}
 default[:rubber][:config][:concurrency] = 20
